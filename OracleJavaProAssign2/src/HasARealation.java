88c462ed211150f668e35b0913908786b2ed8dba
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-07 12:16:36 +0530

    JavaProAssignment 2

---
+++ OracleJavaProAssign2/src/HasARealation.java
@@ -0,0 +1,15 @@
+
+public class HasARealation {
+
+
+	public static void main(String[] args) {		
+		Maruti myMaruti = new Maruti();
+		myMaruti.setColor("RED");
+		myMaruti.setMaxSpeed(180);
+		myMaruti.carInfo();
+		myMaruti.MarutiStartDemo();
+	}
+}
+
+
+
